32b445c3366e3969bce01eb3cda89fb5a751127a
Andrew Johnston <ehjay.github@gmail.com>
2017-05-01 17:36:38 -0400

    allow methods on cors

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func cors(h http.Handler) http.Handler {
     fn := func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
         if (r.Method == "OPTIONS") {
           w.WriteHeader(200)
         } else {
